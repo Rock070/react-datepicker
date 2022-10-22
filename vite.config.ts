279b2b811da3140d87a2bf1faa5acffc1acb3fc3
Rock070 <a0938130702@gmail.com>
2022-10-22 16:03:50 +0800

    build(dep): vite.config, dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Unocss()],
   resolve: {
     alias: {
-      '@': './src',
+      '@': './src'
     }
   }
 })
