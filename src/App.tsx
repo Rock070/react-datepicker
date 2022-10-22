47ad864f5f4e324adb430cf47b88229facd72724
Rock070 <a0938130702@gmail.com>
2022-10-22 16:04:35 +0800

    feat(main): core file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import BasicButton from '@/components/BasicButton';
-import './App.css'
+import BasicCalendar from '@/components/atoms/BasicCalendar'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <BasicButton />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className='flex flex-col items-center space-y-5'>
+        <img src={reactLogo} alt="logo" />
+        <BasicCalendar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
