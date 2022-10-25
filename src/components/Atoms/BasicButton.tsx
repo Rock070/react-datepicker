dcc00d30c1d2b754d091702a442781610e98fcda
Rock070 <a0938130702@gmail.com>
2022-10-25 23:58:22 +0800

    fix: add atoms

---
+++ src/components/Atoms/BasicButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import cx from 'classnames'
+
+interface Props {
+  type?: 'button' | 'submit' | 'reset'
+  className?: string
+  disabled?: boolean
+  onClick?: () => void
+  children?: any
+}
+
+const BasicButton: React.FC<Props> = ({
+  type = 'button',
+  onClick = () => {},
+  className = '',
+  disabled = false,
+  children = ''
+}) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={cx(
+        'py-0.5 px-1 rounded-sm',
+        className,
+        {
+          'cursor-not-allowed pointer-events-none bg-gray-3': disabled,
+          'hover:bg-gray-3': !disabled
+        }
+      )}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default BasicButton
