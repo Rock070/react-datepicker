cec9959e12b60a0cfcbd3110f7c8f8c0ddd21eca
Rock070 <a0938130702@gmail.com>
2022-11-09 00:33:28 +0800

    style(eslint): lintfix, add react-tsx-indent rule

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import OrgCalendar from '@/components/Organisms/OrgCalendar'
 import { Mode } from '@/types'
 
 const App: React.FC = () => {
-  const [mode, changeMode] = useState<Mode>(Mode.DateRange)
-  const onChangeMode = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value as unknown as Mode
-    changeMode(value)
-  }
   const [date, setDate] = useState(new Date())
   const [rangeDate, setRangeDate] = useState([new Date(), new Date()])
 
@@ -18,14 +13,6 @@ const App: React.FC = () => {
       <div className='flex flex-col items-center space-y-5 h-full pt-20'>
         <img src={reactLogo} alt="logo" />
 
-        <h2>mode: {mode}</h2>
-        <select
-          onChange={onChangeMode}
-          value={mode}
-        >
-          <option value={Mode.DatePicker}>DatePicker</option>
-          <option value={Mode.DateRange}>DateRange</option>
-        </select>
         <div className="flex space-x-10 text-center">
           <div>
             <div>Calendar</div>
@@ -35,7 +22,16 @@ const App: React.FC = () => {
               setDate={setDate}
               mode={Mode.DatePicker}
             />
-            </div>
+          </div>
+          <div>
+            <div>Calendar Multiple</div>
+            <div>{date.toLocaleDateString()}</div>
+            <OrgCalendar
+              date={date}
+              setDate={setDate}
+              mode={Mode.DatePicker}
+            />
+          </div>
           <div>
             <div>RangeCalendar</div>
             <div>{rangeDate[0]?.toLocaleDateString()} - {rangeDate[1]?.toLocaleDateString()}</div>
