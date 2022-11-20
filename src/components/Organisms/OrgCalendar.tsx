a922fa44b1373ee013b7e1a2938b51a4d570f52b
Rock070 <a0938130702@gmail.com>
2022-11-20 12:51:12 +0800

    feat!: calendar props `mode` can be optional, error catcher

--- src/components/Organisms/OrgCalendar.tsx
+++ src/components/Organisms/OrgCalendar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useCalendar } from '@/hooks/useCalendar'
 import { useDateRange } from '@/hooks/useDateRange'
 import { useCalendarMultiple } from '@/hooks/useCalendarMultiple'
+import consola from 'consola'
 
 import MolDay from '@/components/Molecules/MolDaysView'
 import MolMonth from '@/components/Molecules/MolMonthsView'
@@ -13,16 +14,29 @@ import { isArray } from '@/utils/is'
 import { ViewMode, Mode } from '@/types'
 
 export interface CalendarProps {
-  mode: Mode
   date: Date | Date[]
   setDate: React.Dispatch<React.SetStateAction<Date>> | React.Dispatch<React.SetStateAction<Date[]>>
+  mode?: Mode
   width?: number
 }
 
 const Calendar: React.FC<CalendarProps> = props => {
-  const { date, setDate, width = 350, mode } = props
+  const { date, setDate, width = 350, mode = Mode.DatePicker } = props
+  consola.log(date, setDate, width, mode)
+  if (date == undefined) {
+    consola.error('date is undefined, it has to be value as Date type with default mode or Date[] type with multiple, range mode')
+    return <></>
+  }
+  if (setDate == undefined) {
+    consola.error('setDate is undefined')
+    return <></>
+  }
+  if (mode == undefined) {
+    consola.error('mode')
+    return <></>
+  }
   if ((mode & Mode.DateRange) && !isArray(date)) {
-    console.error('Date need to be array type')
+    consola.error('Date need to be array type')
     return <></>
   }
 
