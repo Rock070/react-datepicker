8ac1f1c3f2bb60364f8509add143c8d350e65f79
Rock070 <a0938130702@gmail.com>
2022-11-27 21:37:17 +0800

    feat(utils): isNumber

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,2 +1,3 @@
 export const isArray = (p: any) => Array.isArray(p)
 export const isDate = (val: any) => val instanceof Date
+export const isNumber = (val: any): val is number => typeof val === 'number'
