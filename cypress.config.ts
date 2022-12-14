fd76da698a6d12e39f0774350b1d274469101317
Rock070 <a0938130702@gmail.com>
2022-12-14 23:52:15 +0800

    test: 修改

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   defaultCommandTimeout: 10000,
   projectId: 'rb9uzx',
   e2e: {
-    // baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents (on, config) {
       // implement node event listeners here
     }
